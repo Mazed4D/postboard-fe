1033c229d3641626be8da38284801ca80270d87c
Mazed4D <playmyfiddleman@gmail.com>
2022-03-07 18:34:00 +0100

    added loading posts to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,38 @@
-import { Pagination } from 'antd';
+import { Pagination, Spin } from 'antd';
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import PostCard from '../components/post/PostCard';
 
 const Home = () => {
+	const [loading, setLoading] = useState(false);
+	const [numberOfPosts, setNumberOfPosts] = useState(0);
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		setLoading(true);
+		const printPosts = async () => {
+			const loadPosts = await axios.get(`${process.env.REACT_APP_API}/posts`);
+			setPosts(loadPosts.data);
+			setNumberOfPosts(loadPosts.data.length);
+		};
+		printPosts();
+		setLoading(false);
+	}, []);
+
 	return (
 		<div>
 			<h1>Public feed</h1>
-			<PostCard />
-			<PostCard />
-			<PostCard />
-			<PostCard />
+			{loading && <Spin />}
+			{posts.map((post) => {
+				return <PostCard name={post.name} text={post.text} />;
+			})}
 
 			<Pagination
 				defaultCurrent={1}
 				pageSize={4}
 				showSizeChanger={false}
-				total={20}
+				total={numberOfPosts}
 			/>
 		</div>
 	);
