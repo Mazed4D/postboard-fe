ac2f3962a9b106f6afd852353a6ad8a15751f548
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:51:35 +0100

    adding posts now functional

--- src/components/home/AddPost.js
+++ src/components/home/AddPost.js
@@ -1,16 +1,37 @@
 import { Button, Input, Spin } from 'antd';
 import { useState } from 'react';
 import { SendOutlined } from '@ant-design/icons';
-
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 const { TextArea } = Input;
+
+const config = {
+	headers: {
+		Authorization: `Bearer ${localStorage.getItem('token')}`,
+	},
+};
 
 const AddPost = () => {
 	const [text, setText] = useState('');
 	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
 	const sendPostHandler = async () => {
 		setLoading(true);
-		console.log('doen did it ');
+		try {
+			const res = await axios.post(
+				`${process.env.REACT_APP_API}/posts`,
+				{
+					text,
+				},
+				config
+			);
+			navigate(0);
+		} catch (error) {
+			console.log(error.response);
+		}
+
 		setLoading(false);
 	};
 
@@ -21,7 +42,7 @@ const AddPost = () => {
 					showCount
 					maxLength={280}
 					style={{ height: 120 }}
-					onChange={(e) => setText(e.current)}
+					onChange={(e) => setText(e.target.value)}
 					value={text}
 				/>
 				<Button
