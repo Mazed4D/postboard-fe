4bdf4d4f18a8774377f3efbcd0811976561d714a
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:16:04 +0100

    layout component now takes into account authorization

--- src/components/layout/LayoutComp.js
+++ src/components/layout/LayoutComp.js
@@ -1,24 +1,62 @@
 import { Layout, Menu } from 'antd';
 import { NavLink, Outlet } from 'react-router-dom';
+import {
+	HomeOutlined,
+	UserOutlined,
+	LogoutOutlined,
+	MessageFilled,
+} from '@ant-design/icons';
 import styles from './Layout.module.less';
+import Text from 'antd/lib/typography/Text';
+import { useDispatch, useSelector } from 'react-redux';
+import authServices from '../../services/auth.service';
+import { deleteCredientials } from '../../redux/auth';
 
 const { Header, Footer, Content } = Layout;
 
 const LayoutComp = () => {
+	const token = localStorage.getItem('token');
+	const userId = localStorage.getItem('userId');
+	const isLoggedIn = token || false;
+	const dispatch = useDispatch();
+
+	const logoutHandler = () => {
+		authServices.logout();
+		dispatch(deleteCredientials());
+	};
+
 	return (
 		<Layout className='layout' style={{ minHeight: '100vh' }}>
 			<Header>
-				<div className='logo' />
+				<div className={styles.logo}>
+					<Text style={{ fontSize: '1rem' }}>
+						<MessageFilled /> Postboard
+					</Text>
+				</div>
 				<Menu theme='dark' mode='horizontal' defaultSelectedKeys={['2']}>
-					<Menu.Item>
-						<NavLink to={'/'}>Home</NavLink>
-					</Menu.Item>
-					<Menu.Item>
-						<NavLink to={'/user/fakeuserid'}>Profile</NavLink>
-					</Menu.Item>
-					<Menu.Item>
-						<NavLink to={'/auth'}>Authenticate (TEMP)</NavLink>
-					</Menu.Item>
+					{isLoggedIn && (
+						<>
+							<Menu.Item key='home'>
+								<NavLink to={'/'}>
+									<HomeOutlined /> Home
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item key='profile'>
+								<NavLink to={`/user/${userId}`}>
+									<UserOutlined /> Profile
+								</NavLink>
+							</Menu.Item>
+							<Menu.Item
+								key='logout'
+								style={{ marginLeft: 'auto' }}
+								onClick={logoutHandler}
+							>
+								<NavLink to={`/}`}>
+									<LogoutOutlined /> Logout
+								</NavLink>
+							</Menu.Item>
+						</>
+					)}
 				</Menu>
 			</Header>
 			<Content style={{ padding: '50px' }}>
