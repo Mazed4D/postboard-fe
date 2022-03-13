808c73350f1cf272ddfa9185155d24203f162b03
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:17:50 +0100

    profile header modified to use stored token

--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -1,14 +1,36 @@
 import { Avatar } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useState } from 'react';
+
+const config = {
+	headers: {
+		Authorization: `Bearer ${localStorage.getItem('token')}`,
+	},
+};
 
 const ProfileHeader = () => {
-	const params = useParams();
+	const { userId } = useParams();
+	const [name, setName] = useState('Loading...');
+
+	useEffect(() => {
+		const fetchUserName = async () => {
+			const user = await axios.get(
+				`${process.env.REACT_APP_API}/users/${userId}`,
+				config
+			);
+			setName(user.data.name);
+		};
+		fetchUserName();
+	});
+
 	return (
 		<div>
 			<Avatar size='large' shape='square' icon={<UserOutlined />} />
-			<h2>User name</h2>
-			<p>{params.userId}</p>
+			<h2>{name}</h2>
+			<p>{userId}</p>
 		</div>
 	);
 };
