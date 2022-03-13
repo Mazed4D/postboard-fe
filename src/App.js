3b0ad4cbedad762fc44a6605a18a27537d785031
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:18:13 +0100

    app now uses redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import LayoutComp from './components/layout/LayoutComp';
 import 'antd/dist/antd.dark.less';
 import './App.less';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Post from './pages/Post';
+import RegisterPage from './pages/Register';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { deleteCredientials, setCredientials } from './redux/auth';
+import jwtDecode from 'jwt-decode';
+import authServices from './services/auth.service';
 
 function App() {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		const token = localStorage.getItem('token');
+		const user = localStorage.getItem('user');
+		const userId = localStorage.getItem('userId');
+		if (!token) {
+			authServices.logout();
+			dispatch(deleteCredientials());
+		} else {
+			dispatch(
+				setCredientials({
+					isLoggedIn: true,
+					user: user,
+					userId: userId,
+				})
+			);
+		}
+	}, []);
+
+	const { isLoggedIn } = useSelector((state) => state.auth);
+
 	return (
 		<div className='App'>
 			<Routes>
 				<Route path='/' element={<LayoutComp />}>
-					<Route index element={<Home />} />
-					<Route path='auth' element={<AuthPage />} />
-					<Route path='user/:userId' element={<Profile />} />
-					<Route path='post/:postId' element={<Post />} />
+					{isLoggedIn && (
+						<>
+							<Route index element={<Home />} />
+							<Route path='/:page' element={<Home />} />
+							<Route path='user/:userId' element={<Profile />} />
+							<Route path='user/:userId/:page' element={<Profile />} />
+							<Route path='post/:postId' element={<Post />} />
+							<Route path='*' element={<Navigate to='/' />} />
+						</>
+					)}
+					{!isLoggedIn && (
+						<>
+							<Route index element={<AuthPage />} />
+							<Route path='register' element={<RegisterPage />} />
+							<Route path='*' element={<Navigate to='/' />} />
+						</>
+					)}
 				</Route>
 			</Routes>
 		</div>
