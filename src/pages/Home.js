6e981d432e6b9aa599d10cea67e16190d8c25ccf
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:14:55 +0100

    home component now only fetches post ids

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,44 +3,83 @@ import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AddPost from '../components/home/AddPost';
 import PostCard from '../components/post/PostCard';
 
+// TEMPORARY
+const LOGGED_USER_ID = '621a42da50d4990408f57657';
+
 const Home = () => {
+	const navigate = useNavigate();
+	const { page } = useParams();
 	const [loading, setLoading] = useState(false);
 	const [numberOfPosts, setNumberOfPosts] = useState(0);
 	const [posts, setPosts] = useState([]);
+	const [pageNum, setPageNum] = useState(page || 1);
 	useEffect(() => {
 		setLoading(true);
 		const printPosts = async () => {
-			const loadPosts = await axios.get(`${process.env.REACT_APP_API}/posts`);
-			setPosts(loadPosts.data);
-			setNumberOfPosts(loadPosts.data.length);
+			try {
+				const loadPosts = await axios.get(
+					`${process.env.REACT_APP_API}/posts?page=${pageNum}`
+				);
+				setPosts(loadPosts.data.postIds);
+				setNumberOfPosts(loadPosts.data.totalPosts);
+			} catch (error) {
+				toast.error(
+					`${error.response.data.msg || error.response.data} (${
+						error.response.status
+					})`,
+					{
+						position: 'top-right',
+						autoClose: 3000,
+						hideProgressBar: true,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+					}
+				);
+			}
 		};
 		printPosts();
 		setLoading(false);
-	}, []);
+	}, [pageNum]);
 
 	return (
 		<div>
 			<h1>Public feed</h1>
+			<div style={{ marginBottom: '3rem' }}>
+				<AddPost />
+			</div>
 			{loading && <Spin />}
 			{posts.map((post) => {
-				return (
-					<PostCard
-						date={new Date(post.createdAt).toUTCString()}
-						name={post.name}
-						text={post.text}
-						postId={post._id}
-						userId={post.user}
-					/>
-				);
+				return <PostCard key={post} postId={post} />;
 			})}
 
 			<Pagination
-				defaultCurrent={1}
+				current={pageNum}
 				pageSize={4}
 				showSizeChanger={false}
 				total={numberOfPosts}
+				onChange={(e) => {
+					setPageNum(e);
+					navigate(`/${e}`);
+				}}
+			/>
+			<ToastContainer
+				position='top-right'
+				autoClose={3000}
+				hideProgressBar
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
 			/>
 		</div>
 	);
