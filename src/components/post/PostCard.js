80ca82b2a5121103849acd7dd39fea6f099f9836
Mazed4D <playmyfiddleman@gmail.com>
2022-03-13 18:15:15 +0100

    postcarrd component refactored, spun into display card

--- src/components/post/PostCard.js
+++ src/components/post/PostCard.js
@@ -1,42 +1,70 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { Avatar, Card } from 'antd';
+import axios from 'axios';
 import {
-	UserOutlined,
-	EditOutlined,
 	LikeOutlined,
 	LikeFilled,
 	CommentOutlined,
+	EditOutlined,
 } from '@ant-design/icons';
-import axios from 'axios';
-import Text from 'antd/lib/typography/Text';
+import DisplayCard from './DisplayCard';
+import LoadingCard from './LoadingCard';
+import { useSelector } from 'react-redux';
 
+//TEMPORARY
 const config = {
 	headers: {
-		Authorization:
-			'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjFhNDJkYTUwZDQ5OTA0MDhmNTc2NTciLCJuYW1lIjoibWlsYW4iLCJpYXQiOjE2NDY2NzAyNzksImV4cCI6MTY0NzI3NTA3OX0.f0Hr4lFReVnJt5QvHRD5ZO5UI-CjmblQ0CdZ2ISwvOI',
+		Authorization: `Bearer ${localStorage.getItem('token')}`,
 	},
 };
 
-const PostCard = ({
-	isOwnPost = false,
-	userId,
-	date,
-	name = 'User name',
-	text = 'post text',
-	isLiked = false,
-	likes = 0,
-	loading = false,
-	postId,
-}) => {
+const PostCard = ({ postId }) => {
+	// HOOKS
+	const { userId } = useSelector((state) => state.auth);
 	const params = useParams();
-	// POST ID
-	const id = postId || params.postId;
 	const navigate = useNavigate();
-	const [liked, setLiked] = useState(isLiked);
-	const [likeNumber, setLikeNumber] = useState(likes);
+	const [postData, setPostData] = useState();
+	const [liked, setLiked] = useState(false);
+	const [likeNumber, setLikeNumber] = useState();
+	const [likeSpin, setLikeSpin] = useState(false);
+
+	// CONSTANTS
+	const id = postId || params.postId;
+
+	// RUN ON FIRST RENDER
+	useEffect(() => {
+		const loadPost = async () => {
+			const resPost = await axios.get(
+				`${process.env.REACT_APP_API}/posts/${id}`,
+				config
+			);
+			const data = await resPost.data;
+			setPostData(() => {
+				return { ...data };
+			});
+		};
+		const loadLikes = async () => {
+			const resLikes = await axios.get(
+				`${process.env.REACT_APP_API}/likes/${id}`,
+				config
+			);
+			// CHECK IF POST IS LIKED
+			const isLiked = resLikes.data.find((like) => {
+				return like.user === userId;
+			});
+			if (isLiked) {
+				setLiked(true);
+			}
+			setLikeNumber(resLikes.data.length);
+		};
+		loadPost();
+		loadLikes();
+	}, []);
+
+	// LIKE HANDLER FUNCTION
 	const likeHandler = async () => {
 		try {
+			setLikeSpin(true);
 			const toggleLike = await axios.post(
 				`${process.env.REACT_APP_API}/likes/toggle/${id}`,
 				{},
@@ -49,38 +77,66 @@ const PostCard = ({
 				setLikeNumber(likeNumber - 1);
 				setLiked(false);
 			}
+			setLikeSpin(false);
 		} catch (err) {
 			console.log(err.response);
 		}
 	};
 
-	return (
-		<Card
-			style={{ width: 500, marginBottom: '1rem' }}
-			hoverable
-			loading={loading}
-			actions={[
-				<>{isOwnPost && <EditOutlined key='edit' />}</>,
-				<div style={{ display: 'inline-block' }} onClick={likeHandler}>
-					{likeNumber} {liked && <LikeFilled key='like' title='lol' />}
-					{!liked && <LikeOutlined key='like' title='lol' />}
-				</div>,
-				<CommentOutlined
-					key='comments'
-					onClick={() => navigate(`/post/${id}`)}
-				/>,
-			]}
-		>
-			<Card.Meta
-				onClick={() => navigate(`/user/${userId}`)}
-				avatar={<Avatar shape='square' icon={<UserOutlined />} />}
-				title={name}
-				description={date}
+	// ACTIONS
+	const actions = [
+		<div style={{ display: 'inline-block' }} onClick={likeHandler}>
+			{likeNumber}{' '}
+			{liked ? (
+				<LikeFilled key='like' title='lol' spin={likeSpin} />
+			) : (
+				<LikeOutlined key='like' title='lol' spin={likeSpin} />
+			)}
+		</div>,
+		<CommentOutlined key='comments' onClick={() => navigate(`/post/${id}`)} />,
+	];
+
+	if (!postData || likeNumber === undefined) {
+		return <LoadingCard actions={actions} />;
+	}
+	if (postData && likeNumber !== undefined) {
+		// CHECK IF POST IS USER'S OWN POST TO ADD EDIT BUTTON
+		if (postData.user === userId) {
+			actions.unshift(<EditOutlined />);
+		}
+		// RETURN DISPLAY CARD CONTROLLED COMPONENT
+		return (
+			<DisplayCard
+				user={postData.user}
+				key={postData._id}
+				actions={actions}
+				name={postData.name}
+				date={new Date(postData.createdAt).toLocaleString()}
+				postId={id}
+				text={postData.text}
+				navigateUser={() => navigate(`/user/${postData.user}`)}
+				navigatePost={() => navigate(`/post/${id}`)}
 			/>
-			<br />
-			<Text onClick={() => navigate(`/post/${postId}`)}>{text}</Text>
-		</Card>
-	);
+		);
+	}
 };
 
 export default PostCard;
+
+// <Card
+// 	style={{ width: 500, marginBottom: '1rem' }}
+// 	hoverable
+// 	loading={loading}
+// 	actions={actions}
+// >
+// 	<Card.Meta
+// 		onClick={() => navigate(`/user/${postData.user}`)}
+// 		avatar={<Avatar shape='square' icon={<UserOutlined />} />}
+// 		title={postData.name}
+// 		description={new Date(postData.createdAt).toISOString()}
+// 	/>
+// 	<br />
+// 	<Text onClick={() => navigate(`/post/${postId}`)}>
+// 		{postData.text}
+// 	</Text>
+// </Card>
